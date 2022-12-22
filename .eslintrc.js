36ecd3a201f7ec84c897718a4411ada5d26fa3e5
rhuanm <magicknight@hotmail.com.br>
2022-12-22 18:18:04 -0300

    create Navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["@react-native-community", "eslint-config-prettier"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
+import COLORS from "./src/styles/colors";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => (
+  <NavigationContainer>
+    <View style={{ flex: 1 }}>
+      <StatusBar backgroundColor={COLORS.STATUS_BAR} barStyle="light-content" />
+      <Routes />
     </View>
-  );
-}
+  </NavigationContainer>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
